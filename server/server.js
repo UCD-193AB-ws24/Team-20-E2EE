1088a5eb180c390e8466722ba9466b47c904a1e0
DuongHyHenry <duonghyhenry@gmail.com>
2025-02-12 03:34:01 -0800

    Set up Firebase email/password auth

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const admin = require("firebase-admin");
+const credentials = require("./firebase/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials)
+});
+
 const app = express();
 const PORT = process.env.PORT || 5001;
 
@@ -19,6 +26,21 @@ app.post("/api/message", (req, res) => {
     console.log("Received message:", message);
     res.json({ response: `Message Received: ${message}` });
 });
+
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  }
+  const userResponse = await admin.auth().createUser({
+    email: user.email,
+    password: user.password,
+    emailVerified: false, 
+    disabled: false
+  });
+  res.json(userResponse);
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
