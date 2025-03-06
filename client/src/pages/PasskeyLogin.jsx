6bab3b0db61d733a17942a808dfe7d5535473314
Jin Ho Yon <jyon@ucdavis.edu>
2025-03-05 18:03:59 -0800

    Passkey

---
+++ client/src/pages/PasskeyLogin.jsx
@@ -0,0 +1,43 @@
+import {useNavigate} from "react-router-dom"
+import { useCorbado} from "@corbado/react"
+ 
+export default function Profile() {
+    const navigate = useNavigate()
+    const {logout, isAuthenticated, user} = useCorbado();
+ 
+    const redirectToHome = () => {
+        navigate("/")
+    }
+    const handleLogout = () => {
+        logout()
+        redirectToHome()
+    }
+ 
+    if (!isAuthenticated || !user) {
+        return (
+            <div>
+                <p>You're not logged in.</p>
+                <p>
+                    Please go back to{" "}
+                    <a href='/' onClick={redirectToHome}>
+                        home
+                    </a>{" "}
+                    to log in.
+                </p>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>Profile Page</h1>
+                <p>
+                    User-ID: {user.sub}
+                    <br/>
+                    Email: {user.email}
+                </p>
+                <button onClick={handleLogout}>Logout</button>
+            </div>
+ 
+        )
+    }
+}
