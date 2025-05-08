dabd882d2e79d7c586f28bec54e45cd7732ced4b
Jin Ho Yon <jyon@ucdavis.edu>
2025-05-07 17:42:05 -0700

    Changed cors issue

--- server/server.js
+++ server/server.js
@@ -38,9 +38,9 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
     cors({
-      origin: ["http://localhost:5173","https://team-20-e2ee.onrender.com"], // Replace with your frontend URL
+      origin: "https://team-20-e2ee.onrender.com", // Replace with your frontend URL
       methods: "GET, POST, PUT, DELETE", // Allow multiple HTTP methods
-      allowedHeaders: "Content-Type,Authorization", // Allow Content-Type header
+      allowedHeaders: ["Content-Type", "Authorization"], // Allow Content-Type header
       credentials: true // Enable credentials
     })
 );
