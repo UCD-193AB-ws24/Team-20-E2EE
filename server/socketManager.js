1b0cc33f2ec8ff5ce32cc33deba6b27756f79054
Jin Ho Yon <jyon@ucdavis.edu>
2025-05-12 20:53:04 -0700

    fixed conflicting origin issue (https vs http) in server.js vs. socketManager.js

--- server/socketManager.js
+++ server/socketManager.js
@@ -10,7 +10,7 @@ let io = null;
 export const initializeSocket = (httpServer) => {
   io = new Server(httpServer, {
     cors: {
-      origin: "http://ema-chat.com",
+      origin: "https://ema-chat.com",
       methods: ["GET", "POST"],
       credentials: true,
       allowedHeaders: ["Content-Type", "Authorization"],
