6f5ed4c8261ca572676e21996384984440f62649
Jin Ho Yon <jyon@ucdavis.edu>
2025-05-28 14:25:13 -0700

    changed corbado login color (turned off darkmode)

--- client/src/components/CorbadoContextProvider.jsx
+++ client/src/components/CorbadoContextProvider.jsx
@@ -5,7 +5,7 @@ const projectId = import.meta.env.VITE_REACT_APP_CORBADO_PROJECT_ID;
 
 export const CorbadoContextProvider = ({ children }) => {
   return (
-    <CorbadoProvider projectId={projectId}>
+    <CorbadoProvider projectId={projectId} darkMode="off">
       {children}
     </CorbadoProvider>
   );
