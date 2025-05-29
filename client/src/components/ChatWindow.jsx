ec73c5f50b917849e1f842a743a06e8a43637080
Jin Ho Yon <jyon@ucdavis.edu>
2025-05-29 15:56:02 -0700

    Fixed Group Chat Text Bubble Size

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -243,7 +243,7 @@ export default function ChatWindow({ messages, selectedUser, selectedUserID }) {
 
                 {/* Message bubble */}
                 <div
-                  className={'p-3 max-w-[75%] rounded-lg'}
+                  className={'p-3 max-w-[100%] rounded-lg'}
                   style={{ backgroundColor: isMe ? theme.colors.chatBubble.primary : theme.colors.chatBubble.secondary }}
                 >
                   <p>{msg.text}</p>
